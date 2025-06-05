23832f154251e7027842538cc6e6f8c03710eb8c
sona-devassure <sona@devassure.io>
2025-06-05 19:13:10 +0530

    Removed a util function to extract name from playlist

--- parse_utils.py
+++ parse_utils.py
@@ -3,21 +3,11 @@ import plistlib
 def parse_tracks_from_xml(xml_file):
     with open(xml_file, 'rb') as f:
         plist = plistlib.load(f)
-
     tracks_dict = plist.get('Tracks', {})
     tracks = []
-
     for track_id, track_info in tracks_dict.items():
         name = track_info.get('Name')
         artist = track_info.get('Artist')
         if name and artist:
             tracks.append({'name': name, 'artist': artist})
     return tracks
-
-def parse_playlist_name_from_xml(xml_file):
-    with open(xml_file, 'rb') as f:
-        plist = plistlib.load(f)
-    playlists = plist.get('Playlists', [])
-    if playlists and 'Name' in playlists[0]:
-        return playlists[0]['Name']
-    return None
